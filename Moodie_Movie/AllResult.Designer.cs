c06661e54f8f5d2b58b19f2ce80a433ed300ff0b
dltjdud_0225 <s2331@e-mirim.hs.kr>
2025-09-01 10:55:09 +0900

    #2 선택지 1~3개 제한, 결과 출력, 결과 메시지 설정, 스크롤 기능 등 구현

---
+++ Moodie_Movie/AllResult.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace Moodie_Movie
+{
+    partial class AllResult
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(AllResult));
+            this.all_result = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.close_button = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // all_result
+            // 
+            this.all_result.AutoSize = true;
+            this.all_result.Location = new System.Drawing.Point(141, 12);
+            this.all_result.Name = "all_result";
+            this.all_result.Size = new System.Drawing.Size(513, 4548);
+            this.all_result.TabIndex = 4;
+            this.all_result.Text = resources.GetString("all_result.Text");
+            this.all_result.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.AutoScroll = true;
+            this.panel1.Controls.Add(this.all_result);
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(776, 978);
+            this.panel1.TabIndex = 5;
+            // 
+            // close_button
+            // 
+            this.close_button.Location = new System.Drawing.Point(692, 957);
+            this.close_button.Name = "close_button";
+            this.close_button.Size = new System.Drawing.Size(75, 23);
+            this.close_button.TabIndex = 6;
+            this.close_button.Text = "닫기";
+            this.close_button.UseVisualStyleBackColor = true;
+            this.close_button.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // AllResult
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 1002);
+            this.Controls.Add(this.close_button);
+            this.Controls.Add(this.panel1);
+            this.Name = "AllResult";
+            this.Text = "AllResult";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label all_result;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button close_button;
+    }
+}
--- Moodie_Movie/Form1.Designer.cs
+++ Moodie_Movie/Form1.Designer.cs
@@ -28,49 +28,65 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
-            this.checkBox1 = new System.Windows.Forms.CheckBox();
-            this.이름 = new System.Windows.Forms.TextBox();
+            this.resultButton = new System.Windows.Forms.Button();
+            this.check_happe = new System.Windows.Forms.CheckBox();
+            this.tbname = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
-            this.checkBox2 = new System.Windows.Forms.CheckBox();
-            this.checkBox3 = new System.Windows.Forms.CheckBox();
-            this.checkBox4 = new System.Windows.Forms.CheckBox();
-            this.checkBox5 = new System.Windows.Forms.CheckBox();
-            this.checkBox6 = new System.Windows.Forms.CheckBox();
-            this.checkBox7 = new System.Windows.Forms.CheckBox();
+            this.check_sad = new System.Windows.Forms.CheckBox();
+            this.check_anger = new System.Windows.Forms.CheckBox();
+            this.check_excitement = new System.Windows.Forms.CheckBox();
+            this.check_tranquility = new System.Windows.Forms.CheckBox();
+            this.check_melancholy = new System.Windows.Forms.CheckBox();
+            this.check_excited = new System.Windows.Forms.CheckBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.파일ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.result_list = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.끝내기ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.도움ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.표츈텔러정보ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.tbResult = new System.Windows.Forms.TextBox();
+            this.menuStrip1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
             this.SuspendLayout();
             // 
-            // button1
+            // resultButton
             // 
-            this.button1.Location = new System.Drawing.Point(311, 368);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(161, 28);
-            this.button1.TabIndex = 1;
-            this.button1.Text = "작성 완료";
-            this.button1.UseVisualStyleBackColor = true;
+            this.resultButton.Location = new System.Drawing.Point(101, 331);
+            this.resultButton.Name = "resultButton";
+            this.resultButton.Size = new System.Drawing.Size(161, 28);
+            this.resultButton.TabIndex = 1;
+            this.resultButton.Text = "결과 보기";
+            this.resultButton.UseVisualStyleBackColor = true;
+            this.resultButton.Click += new System.EventHandler(this.button1_Click);
             // 
-            // checkBox1
+            // check_happe
             // 
-            this.checkBox1.AutoSize = true;
-            this.checkBox1.Location = new System.Drawing.Point(260, 194);
-            this.checkBox1.Name = "checkBox1";
-            this.checkBox1.Size = new System.Drawing.Size(86, 16);
-            this.checkBox1.TabIndex = 2;
-            this.checkBox1.Text = "checkBox1";
-            this.checkBox1.UseVisualStyleBackColor = true;
+            this.check_happe.AutoSize = true;
+            this.check_happe.Location = new System.Drawing.Point(16, 170);
+            this.check_happe.Name = "check_happe";
+            this.check_happe.Size = new System.Drawing.Size(48, 16);
+            this.check_happe.TabIndex = 2;
+            this.check_happe.Text = "기쁨";
+            this.check_happe.UseVisualStyleBackColor = true;
+            this.check_happe.CheckedChanged += new System.EventHandler(this.check_happe_CheckedChanged);
             // 
-            // 이름
+            // tbname
             // 
-            this.이름.Location = new System.Drawing.Point(380, 101);
-            this.이름.Name = "이름";
-            this.이름.Size = new System.Drawing.Size(100, 21);
-            this.이름.TabIndex = 4;
+            this.tbname.Location = new System.Drawing.Point(136, 77);
+            this.tbname.Name = "tbname";
+            this.tbname.Size = new System.Drawing.Size(100, 21);
+            this.tbname.TabIndex = 4;
+            this.tbname.TextChanged += new System.EventHandler(this.tbname_TextChanged);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(333, 104);
+            this.label1.Location = new System.Drawing.Point(89, 80);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(41, 12);
             this.label1.TabIndex = 5;
@@ -79,107 +95,217 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(333, 154);
+            this.label2.Location = new System.Drawing.Point(89, 130);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(173, 12);
             this.label2.TabIndex = 6;
             this.label2.Text = "감정을 선택하세요(최대 3개) : ";
             // 
-            // checkBox2
+            // check_sad
             // 
-            this.checkBox2.AutoSize = true;
-            this.checkBox2.Location = new System.Drawing.Point(380, 194);
-            this.checkBox2.Name = "checkBox2";
-            this.checkBox2.Size = new System.Drawing.Size(86, 16);
-            this.checkBox2.TabIndex = 7;
-            this.checkBox2.Text = "checkBox2";
-            this.checkBox2.UseVisualStyleBackColor = true;
+            this.check_sad.AutoSize = true;
+            this.check_sad.Location = new System.Drawing.Point(136, 170);
+            this.check_sad.Name = "check_sad";
+            this.check_sad.Size = new System.Drawing.Size(48, 16);
+            this.check_sad.TabIndex = 7;
+            this.check_sad.Text = "슬픔";
+            this.check_sad.UseVisualStyleBackColor = true;
+            this.check_sad.CheckedChanged += new System.EventHandler(this.check_sad_CheckedChanged);
             // 
-            // checkBox3
+            // check_anger
             // 
-            this.checkBox3.AutoSize = true;
-            this.checkBox3.Location = new System.Drawing.Point(506, 194);
-            this.checkBox3.Name = "checkBox3";
-            this.checkBox3.Size = new System.Drawing.Size(86, 16);
-            this.checkBox3.TabIndex = 8;
-            this.checkBox3.Text = "checkBox3";
-            this.checkBox3.UseVisualStyleBackColor = true;
+            this.check_anger.AutoSize = true;
+            this.check_anger.Location = new System.Drawing.Point(262, 170);
+            this.check_anger.Name = "check_anger";
+            this.check_anger.Size = new System.Drawing.Size(48, 16);
+            this.check_anger.TabIndex = 8;
+            this.check_anger.Text = "분노";
+            this.check_anger.UseVisualStyleBackColor = true;
+            this.check_anger.CheckedChanged += new System.EventHandler(this.check_anger_CheckedChanged);
             // 
-            // checkBox4
+            // check_excitement
             // 
-            this.checkBox4.AutoSize = true;
-            this.checkBox4.Location = new System.Drawing.Point(260, 228);
-            this.checkBox4.Name = "checkBox4";
-            this.checkBox4.Size = new System.Drawing.Size(86, 16);
-            this.checkBox4.TabIndex = 9;
-            this.checkBox4.Text = "checkBox4";
-            this.checkBox4.UseVisualStyleBackColor = true;
+            this.check_excitement.AutoSize = true;
+            this.check_excitement.Location = new System.Drawing.Point(16, 204);
+            this.check_excitement.Name = "check_excitement";
+            this.check_excitement.Size = new System.Drawing.Size(52, 16);
+            this.check_excitement.TabIndex = 9;
+            this.check_excitement.Text = " 설렘";
+            this.check_excitement.UseVisualStyleBackColor = true;
+            this.check_excitement.CheckedChanged += new System.EventHandler(this.check_excitement_CheckedChanged);
             // 
-            // checkBox5
+            // check_tranquility
             // 
-            this.checkBox5.AutoSize = true;
-            this.checkBox5.Location = new System.Drawing.Point(380, 228);
-            this.checkBox5.Name = "checkBox5";
-            this.checkBox5.Size = new System.Drawing.Size(86, 16);
-            this.checkBox5.TabIndex = 10;
-            this.checkBox5.Text = "checkBox5";
-            this.checkBox5.UseVisualStyleBackColor = true;
+            this.check_tranquility.AutoSize = true;
+            this.check_tranquility.Location = new System.Drawing.Point(136, 204);
+            this.check_tranquility.Name = "check_tranquility";
+            this.check_tranquility.Size = new System.Drawing.Size(48, 16);
+            this.check_tranquility.TabIndex = 10;
+            this.check_tranquility.Text = "평온";
+            this.check_tranquility.UseVisualStyleBackColor = true;
+            this.check_tranquility.CheckedChanged += new System.EventHandler(this.check_tranquility_CheckedChanged);
             // 
-            // checkBox6
+            // check_melancholy
             // 
-            this.checkBox6.AutoSize = true;
-            this.checkBox6.Location = new System.Drawing.Point(506, 228);
-            this.checkBox6.Name = "checkBox6";
-            this.checkBox6.Size = new System.Drawing.Size(86, 16);
-            this.checkBox6.TabIndex = 11;
-            this.checkBox6.Text = "checkBox6";
-            this.checkBox6.UseVisualStyleBackColor = true;
+            this.check_melancholy.AutoSize = true;
+            this.check_melancholy.Location = new System.Drawing.Point(262, 204);
+            this.check_melancholy.Name = "check_melancholy";
+            this.check_melancholy.Size = new System.Drawing.Size(48, 16);
+            this.check_melancholy.TabIndex = 11;
+            this.check_melancholy.Text = "우울";
+            this.check_melancholy.UseVisualStyleBackColor = true;
+            this.check_melancholy.CheckedChanged += new System.EventHandler(this.check_melancholy_CheckedChanged);
             // 
-            // checkBox7
+            // check_excited
             // 
-            this.checkBox7.AutoSize = true;
-            this.checkBox7.Location = new System.Drawing.Point(260, 262);
-            this.checkBox7.Name = "checkBox7";
-            this.checkBox7.Size = new System.Drawing.Size(86, 16);
-            this.checkBox7.TabIndex = 12;
-            this.checkBox7.Text = "checkBox7";
-            this.checkBox7.UseVisualStyleBackColor = true;
+            this.check_excited.AutoSize = true;
+            this.check_excited.Location = new System.Drawing.Point(16, 238);
+            this.check_excited.Name = "check_excited";
+            this.check_excited.Size = new System.Drawing.Size(48, 16);
+            this.check_excited.TabIndex = 12;
+            this.check_excited.Text = "신남";
+            this.check_excited.UseVisualStyleBackColor = true;
+            this.check_excited.CheckedChanged += new System.EventHandler(this.check_excited_CheckedChanged);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.파일ToolStripMenuItem,
+            this.도움ToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 13;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // 파일ToolStripMenuItem
+            // 
+            this.파일ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.result_list,
+            this.toolStripSeparator1,
+            this.끝내기ToolStripMenuItem});
+            this.파일ToolStripMenuItem.Name = "파일ToolStripMenuItem";
+            this.파일ToolStripMenuItem.Size = new System.Drawing.Size(43, 20);
+            this.파일ToolStripMenuItem.Text = "파일";
+            // 
+            // result_list
+            // 
+            this.result_list.Name = "result_list";
+            this.result_list.Size = new System.Drawing.Size(166, 22);
+            this.result_list.Text = "모든 선택지 결과";
+            this.result_list.Click += new System.EventHandler(this.result_list_Click);
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(163, 6);
+            // 
+            // 끝내기ToolStripMenuItem
+            // 
+            this.끝내기ToolStripMenuItem.Name = "끝내기ToolStripMenuItem";
+            this.끝내기ToolStripMenuItem.Size = new System.Drawing.Size(166, 22);
+            this.끝내기ToolStripMenuItem.Text = "끝내기";
+            this.끝내기ToolStripMenuItem.Click += new System.EventHandler(this.끝내기ToolStripMenuItem_Click);
+            // 
+            // 도움ToolStripMenuItem
+            // 
+            this.도움ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.표츈텔러정보ToolStripMenuItem});
+            this.도움ToolStripMenuItem.Name = "도움ToolStripMenuItem";
+            this.도움ToolStripMenuItem.Size = new System.Drawing.Size(55, 20);
+            this.도움ToolStripMenuItem.Text = "도움말";
+            // 
+            // 표츈텔러정보ToolStripMenuItem
+            // 
+            this.표츈텔러정보ToolStripMenuItem.Name = "표츈텔러정보ToolStripMenuItem";
+            this.표츈텔러정보ToolStripMenuItem.Size = new System.Drawing.Size(150, 22);
+            this.표츈텔러정보ToolStripMenuItem.Text = "표츈텔러 정보";
+            this.표츈텔러정보ToolStripMenuItem.Click += new System.EventHandler(this.표츈텔러정보ToolStripMenuItem_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.tbname);
+            this.groupBox1.Controls.Add(this.check_excited);
+            this.groupBox1.Controls.Add(this.resultButton);
+            this.groupBox1.Controls.Add(this.check_melancholy);
+            this.groupBox1.Controls.Add(this.check_happe);
+            this.groupBox1.Controls.Add(this.check_tranquility);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.check_excitement);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.check_anger);
+            this.groupBox1.Controls.Add(this.check_sad);
+            this.groupBox1.Location = new System.Drawing.Point(12, 27);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(367, 401);
+            this.groupBox1.TabIndex = 14;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "사용자 정보 입력";
+            this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.tbResult);
+            this.groupBox2.Location = new System.Drawing.Point(385, 27);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(403, 401);
+            this.groupBox2.TabIndex = 15;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "결과 보기";
+            // 
+            // tbResult
+            // 
+            this.tbResult.Location = new System.Drawing.Point(6, 20);
+            this.tbResult.Multiline = true;
+            this.tbResult.Name = "tbResult";
+            this.tbResult.Size = new System.Drawing.Size(391, 366);
+            this.tbResult.TabIndex = 1;
+            this.tbResult.TextChanged += new System.EventHandler(this.tbResult_TextChanged);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.checkBox7);
-            this.Controls.Add(this.checkBox6);
-            this.Controls.Add(this.checkBox5);
-            this.Controls.Add(this.checkBox4);
-            this.Controls.Add(this.checkBox3);
-            this.Controls.Add(this.checkBox2);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.이름);
-            this.Controls.Add(this.checkBox1);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
             this.Name = "Form1";
             this.Text = "Moodie_Movie";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
         }
 
         #endregion
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.CheckBox checkBox1;
-        private System.Windows.Forms.TextBox 이름;
+        private System.Windows.Forms.Button resultButton;
+        private System.Windows.Forms.CheckBox check_happe;
+        private System.Windows.Forms.TextBox tbname;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.CheckBox checkBox2;
-        private System.Windows.Forms.CheckBox checkBox3;
-        private System.Windows.Forms.CheckBox checkBox4;
-        private System.Windows.Forms.CheckBox checkBox5;
-        private System.Windows.Forms.CheckBox checkBox6;
-        private System.Windows.Forms.CheckBox checkBox7;
+        private System.Windows.Forms.CheckBox check_sad;
+        private System.Windows.Forms.CheckBox check_anger;
+        private System.Windows.Forms.CheckBox check_excitement;
+        private System.Windows.Forms.CheckBox check_tranquility;
+        private System.Windows.Forms.CheckBox check_melancholy;
+        private System.Windows.Forms.CheckBox check_excited;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem 파일ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 끝내기ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 도움ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 표츈텔러정보ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem result_list;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox tbResult;
     }
 }
 
